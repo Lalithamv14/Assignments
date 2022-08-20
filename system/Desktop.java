e4b377720a61cb8f597961ad5d195028de165ce9
Lalithamv14 <110729396+Lalithamv14@users.noreply.github.com>
2022-08-20 21:02:44 +0530

    Add files via upload

---
+++ system/Desktop.java
@@ -0,0 +1,16 @@
+package org.system;
+
+public class Desktop extends Computer{
+
+public void desktopsize()	
+	{
+		System.out.println("Desktop size is 29 to 30 inches");
+	}
+	
+	public static void main(String[] args) {
+		Desktop call=new Desktop();
+		call.computerModel();
+		call.desktopsize();
+		
+	}
+}
